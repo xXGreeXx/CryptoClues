bffae9926b4ed5304085aef8afb6f5245f334007
Li Smith <lrhude.elijah@gmail.com>
2017-12-21 17:15:11 -0500

    Added alot of new stuff
    Added startscreen and started game scene

---
+++ CryptoClues/Assets/Scripts/StartScreenTextHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenTextHandler : MonoBehaviour {
+
+    GameObject text;
+
+	// Use this for initialization
+	void Start () {
+
+        text = GameObject.Find("Text");
+
+        StartCoroutine(waiter());
+	}
+	
+    //add text
+    IEnumerator waiter()
+    {
+        text.GetComponent<UnityEngine.UI.Text>().text += "Initializing systems.... \n";
+        yield return new WaitForSeconds(2.5F);
+
+        text.GetComponent<UnityEngine.UI.Text>().text += "Systems ready \n";
+        yield return new WaitForSeconds(1);
+
+        text.GetComponent<UnityEngine.UI.Text>().text += "Loading puzzles  \n";
+        for (int i = 0; i < 30; i++)
+        {
+            text.GetComponent<UnityEngine.UI.Text>().text += "Crypto: " + Random.Range(0, 3) + Random.Range(0, 3) + Random.Range(0, 3) + "\n";
+            yield return new WaitForSeconds(0.01F);
+        }
+        text.GetComponent<UnityEngine.UI.Text>().text += "Puzzles ready  \n";
+        yield return new WaitForSeconds(0.5F);
+
+        text.GetComponent<UnityEngine.UI.Text>().text += "Building UI.... \n";
+        yield return new WaitForSeconds(1.5F);
+
+        text.GetComponent<UnityEngine.UI.Text>().text += "UI Ready \n";
+        yield return new WaitForSeconds(1);
+
+        text.GetComponent<UnityEngine.UI.Text>().text += "Booting main drive... \n";
+        yield return new WaitForSeconds(2);
+
+        text.GetComponent<UnityEngine.UI.Text>().text += "All systems ready \n";
+    }
+}
