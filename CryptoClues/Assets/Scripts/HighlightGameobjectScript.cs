5e89af369bc3d2923225ab195f43b4f3a845667a
Li Smith <lrhude.elijah@gmail.com>
2017-12-21 22:38:27 -0500

    Added alot

---
+++ CryptoClues/Assets/Scripts/HighlightGameobjectScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightGameobjectScript : MonoBehaviour {
+    Color startColor = Color.white;
+
+    void OnMouseEnter()
+    {
+        startColor = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = startColor;
+    }
+}
